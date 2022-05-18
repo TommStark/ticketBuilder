65e8bfce57dda92e47ebbe053eb352983d867e55
TommStark <posik.tomas@gmail.com>
2022-05-18 12:32:53 -0300

    added new percentage feature in dashboard view

--- src/modules/dashboard/DashBoardTotalTickets.js
+++ src/modules/dashboard/DashBoardTotalTickets.js
@@ -1,70 +1,125 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { Avatar, Box, Card, CardContent, Grid, Typography } from '@mui/material';
+import { Avatar, Box, Card, CardContent, Grid, Typography, LinearProgress} from '@mui/material';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 
-export const DashBoardTotalTickets = (props) => (
-    <Card
-        sx={{ height: '100%' }}
-    >
-        <CardContent>
-            <Grid
-                container
-                spacing={3}
-                sx={{ justifyContent: 'space-between' }}
-            >
-                <Grid item>
-                    <Typography
-                        color="textSecondary"
-                        gutterBottom
-                        variant="overline"
-                    >
-                        {props.title}
-                    </Typography>
-                    <Typography
-                        color="textPrimary"
-                        variant="h5"
-                    >
-                        {props.total}
-                    </Typography>
-                </Grid>
-                <Grid item>
-                    <Avatar
-                        sx={{
-                            backgroundColor : 'primary.main',
-                            height          : 56,
-                            width           : 56
-                        }}
-                    >
-                        {props.icon}
-                    </Avatar>
-                </Grid>
-            </Grid>
-            {/* <Box
-                sx={{
-                    pt         : 2,
-                    display    : 'flex',
-                    alignItems : 'center'
-                }}
-            >
-                <ArrowDownwardIcon color="error" />
-                <Typography
-                    color="error"
-                    sx={{
-                        mr: 1
-                    }}
-                    variant="body2"
+export const DashBoardTotalTickets = (props) => {
+    let isPositive;
+    let porcentaje;
+    let diff;
+
+    if(props.showPorcentage){
+        const pastMonth = new Date().getMonth();
+    
+        let pastMonthTickets = props.allTickets.filter(ticket => new Date(ticket.start_date).getMonth()+1 === pastMonth);
+    
+        const thisMonthTickets = props.tickets.length || 1;
+        pastMonthTickets = pastMonthTickets.length;
+    
+        isPositive = thisMonthTickets > pastMonthTickets;
+
+        if (pastMonthTickets > 0){
+            if(thisMonthTickets > pastMonthTickets){
+                diff = thisMonthTickets - pastMonthTickets;
+            }else if( thisMonthTickets < pastMonthTickets ){
+                diff = pastMonthTickets - thisMonthTickets;
+            }
+            
+            porcentaje = Math.round((diff / pastMonthTickets)*100);
+        }else{
+            porcentaje = Math.round(thisMonthTickets*100);
+        }
+    }
+    if(props.teamTotal && props.total > 0){        
+        porcentaje = Math.round((props.total * 100)/ props.teamTotal);
+    }
+
+    return (
+        <Card
+            sx={{ height: '100%' }}
+        >
+            <CardContent>
+                <Grid
+                    container
+                    spacing={3}
+                    sx={{ justifyContent: 'space-between' }}
                 >
-          12%
-                </Typography>
-                <Typography
-                    color="textSecondary"
-                    variant="caption"
-                >
-          Since last month
-                </Typography>
-            </Box> */}
-        </CardContent>
-    </Card>
-);
+                    <Grid item>
+                        <Typography
+                            color="textSecondary"
+                            gutterBottom
+                            variant="overline"
+                        >
+                            {props.title}
+                        </Typography>
+                        <Typography
+                            color="textPrimary"
+                            variant="h4"
+                        >
+                            {props.total} Tickets
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Avatar
+                            sx={{
+                                backgroundColor : `${props.color}.main`,
+                                height          : 56,
+                                width           : 56
+                            }}
+                        >
+                            {props.icon}
+                        </Avatar>
+                    </Grid>
+                </Grid>
+                {
+                    props.showPorcentage && props.total !== 0
+                        ?
+                        <Box
+                            sx={{
+                                pt         : 2,
+                                display    : 'flex',
+                                alignItems : 'center'
+                            }}
+                        >
+                            {   isPositive
+                                ?
+                                <ArrowUpwardIcon color="success" />
+                                :
+                                <ArrowDownwardIcon color='error' />
+                            }
+                            <Typography
+                                color={isPositive ? 'green' : 'error'}
+                                sx={{
+                                    mr: 1
+                                }}
+                                variant="body2"
+                            >
+                                {porcentaje}%
+                            </Typography>
+                            <Typography
+                                color="textSecondary"
+                                variant="caption"
+                            >
+                                Since last month
+                            </Typography>
+                        </Box>
+
+                        :  null
+                }
+                {
+                    props.teamTotal && props.total > 0
+                        ?
+                        <Box sx={{ pt: 3 }}>
+                            <LinearProgress
+                                value={porcentaje || 100}
+                                variant="determinate"
+                            />
+                        </Box>
+                        : null
+                }  
+            </CardContent>
+        </Card>
+    );
+};
--- src/modules/dashboard/DashboardContainer.js
+++ src/modules/dashboard/DashboardContainer.js
@@ -21,6 +21,9 @@ function DashboardContainer () {
     const graph = useSelector((state)=> state.DashBoard.projectsByUsers.graph);
     const x = tickets.map(t =>  new Date(Date.parse(t.start_date)).toJSON().slice(0,10));
     const y = teamTickes.map(t =>  new Date(Date.parse(t.start_date)).toJSON().slice(0,10));
+    const thisMonth = new Date().getMonth()+1;
+    const thisMonthTickets = tickets.filter(ticket => new Date(ticket.start_date).getMonth()+1 === thisMonth);
+    const thisMonthTeamTickets = teamTickes.filter(ticket => new Date(ticket.start_date).getMonth()+1 === thisMonth);
 
     useEffect(()=>{
         (!!(colors.length || projectsStatus.length) && setIsLoading(false));
@@ -56,7 +59,7 @@ function DashboardContainer () {
                             item
                             lg={12}
                             sm={12}
-                            xl={6}
+                            xl={12}
                             xs={12}
                         >
                             {
@@ -80,9 +83,58 @@ function DashboardContainer () {
                                     ? <Skeleton  variant="rectangular" height={'12vh'} sx={{margin: 0}}/>
                                     :
                                     <DashBoardTotalTickets
-                                        total={tickets.length}
-                                        title={'Your Tickets'}
+                                        total={thisMonthTickets.length}
+                                        title={'Your Tickets this month'}
                                         icon={<PersonIcon/>}
+                                        tickets={thisMonthTickets}
+                                        color={'info'}
+                                        allTickets={tickets}
+                                        showPorcentage={true}
+                                    />
+                            }
+                        </Grid>
+                        <Grid
+                            item
+                            xl={3}
+                            lg={6}
+                            sm={6}
+                            xs={12}
+                        >
+                            {
+                                isLoading
+                                    ? <Skeleton  variant="rectangular" height={'12vh'} sx={{margin: 0}}/>
+                                    :
+                                    <DashBoardTotalTickets
+                                        total={tickets.length}
+                                        title={'Your Tickets TOTAL'}
+                                        icon={<PersonIcon/>}
+                                        tickets={tickets}
+                                        color={'error'}
+                                        allTickets={tickets}
+                                        showPorcentage={false}
+                                        teamTotal={teamTickes.length}
+                                    />
+                            }
+                        </Grid>
+                        <Grid
+                            item
+                            xl={3}
+                            lg={6}
+                            sm={6}
+                            xs={12}
+                        >
+                            {
+                                isLoading
+                                    ? <Skeleton variant="rectangular" height={'12vh'} sx={{margin: 0}}/>
+                                    :
+                                    <DashBoardTotalTickets
+                                        total={thisMonthTeamTickets.length}
+                                        title={'Team Tickets this month'}
+                                        icon={<GroupIcon/>}
+                                        tickets={thisMonthTeamTickets}
+                                        allTickets={teamTickes}
+                                        color={'secondary'}
+                                        showPorcentage={true}
                                     />
                             }
                         </Grid>
@@ -99,11 +151,17 @@ function DashboardContainer () {
                                     :
                                     <DashBoardTotalTickets
                                         total={teamTickes.length}
-                                        title={'Team Tickets'}
+                                        title={'Team Tickets total'}
                                         icon={<GroupIcon/>}
+                                        tickets={teamTickes}
+                                        color={'warning'}
+                                        allTickets={teamTickes}
+                                        showPorcentage={false}
+                                        teamTotal={teamTickes.length}
                                     />
                             }
                         </Grid>
+
                         <Grid
                             item
                             lg={8}
